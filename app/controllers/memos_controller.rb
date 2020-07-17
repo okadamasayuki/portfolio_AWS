9d5ebf9e7eb17f46a354704719d55fc1cac32c1f
EC2 Default User <ec2-user@ip-172-31-35-130.ap-northeast-1.compute.internal>
2020-07-17 05:20:12 +0000

    1420

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -12,4 +12,22 @@ class MemosController < ApplicationController
         Memo.create(title:params["memos"]["title"],content:params["memos"]["content"])
         redirect_to "/"
     end
+    
+    def destroy
+        memo=Memo.find(params["id"])
+        memo.destroy
+        redirect_to "/"
+    end
+    
+    def edit
+        @memo=Memo.find(params["id"])
+    end
+    
+    def update
+        memo=Memo.find(params["id"])
+        memo.title=params["memos"]["title"]
+        memo.content=params["memos"]["content"]
+        memo.save
+        redirect_to "/"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
     get "/", to:"memos#index"
     get "/new", to:"memos#new"
     post "/create",to:"memos#create"
+    delete "/delete/:id",to:"memos#destroy"
+    get "/edit/:id",to:"memos#edit"
+    patch "/update/:id",to:"memos#update"
 end
